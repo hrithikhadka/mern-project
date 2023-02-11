df29e71a4a3cb26b9509decf875b146c0e86b936
Hrithik Khadka <hritik.khadka@centria.fi>
2023-02-11 22:02:42 +0200

    setup routes

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,6 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.get("/api/goals", (req, res) => {
-  res.status(200).json({ message: "Get Goals" });
-});
+app.use("/api/goals", require("./routes/goalRoutes"));
 
 app.listen(port, () => console.log(`server started on port ${port}`));
