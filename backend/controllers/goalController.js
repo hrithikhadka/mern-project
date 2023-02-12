5b23e09319cf3092ef87ba3172cfd8889d7e1ce6
Hrithik Khadka <hritik.khadka@centria.fi>
2023-02-12 21:57:06 +0200

    test and accept body data

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -5,10 +5,12 @@ const getGoals = (req, res) => {
   res.status(200).json({ message: "Get Goals" });
 };
 
-//@desc   Set Goals
+//@desc   Set Goal
 //@route  Post /api/goals
 //@access Private
 const setGoal = (req, res) => {
+  console.log(req.body);
+
   res.status(200).json({ message: "Set Goal" });
 };
 
