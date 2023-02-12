aabf68de1f25c8fe024f484d98731528dd0a3bb5
Hrithik Khadka <hritik.khadka@centria.fi>
2023-02-12 15:06:39 +0200

    create controllers

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,34 @@
+//@desc   Get Goals
+//@route  Get /api/goals
+//@access Private
+const getGoals = (req, res) => {
+  res.status(200).json({ message: "Get Goals" });
+};
+
+//@desc   Set Goals
+//@route  Post /api/goals
+//@access Private
+const setGoal = (req, res) => {
+  res.status(200).json({ message: "Set Goal" });
+};
+
+//@desc   Update Goal
+//@route  PUT /api/goals/:id
+//@access Private
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+};
+
+//@desc   Delete Goal
+//@route  Delete /api/goals/:id
+//@access Private
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+};
+
+module.exports = {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
