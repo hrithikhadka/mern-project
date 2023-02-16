db5eb602b4b0edf45944b6fe4e6d76907218445c
Hrithik Khadka <hritik.khadka@centria.fi>
2023-02-16 08:12:52 +0200

    delete Goal and test

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -47,7 +47,20 @@ const updateGoal = asyncHandler(async (req, res) => {
 //@route  Delete /api/goals/:id
 //@access Private
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  await goal.remove();
+
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
