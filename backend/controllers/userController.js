93d125d3fa56bc4a26ca880e332d50511d53976c
Hrithik Khadka <hritik.khadka@centria.fi>
2023-02-18 22:30:59 +0200

    register user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,26 +1,46 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
 //@desc     Register new user
 //@route    POST /api/users
 //@access   Public
 
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+
+  //check if user exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error8("user already exists");
+  }
+
   res.json({ message: "register user" });
-};
+});
 
 //@desc     Authenticate a user
 //@route    POST /api/users/login
 //@access   Public
 
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.json({ message: "login user" });
-};
+});
 
 //@desc     Get user data
 //@route    GET /api/users/me
 //@access   Public
 
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
   res.json({ message: "User data display" });
-};
+});
 
 module.exports = {
   registerUser,
